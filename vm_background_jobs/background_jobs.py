a64654d8e68304fc1c40c3b44bb1ef808dc71ce9
darkh14 <dark_h14@mail.ru>
2022-11-30 21:35:56 +0300

    test job error 3

--- vm_background_jobs/background_jobs.py
+++ vm_background_jobs/background_jobs.py
@@ -147,7 +147,7 @@ class BackgroundJob:
                                             self._id,
                                             self._job_name,
                                             self._db_connector.db_path], stdout=f_out, stderr=f_err)
-            print(job_process)
+            print('job-process - {}'.format(job_process))
 
         self._pid = job_process.pid
         self._write_to_db()
