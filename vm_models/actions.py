82050d780ac746d824782d227c7ea8c9270f2ad9
darkh14 <dark_h14@mail.ru>
2022-12-01 15:42:10 +0300

    add model actions

--- vm_models/actions.py
+++ vm_models/actions.py
@@ -7,9 +7,37 @@
 
 __all__ = ['get_actions']
 
-from typing import Callable
+from typing import Callable, Any
 
 
 def get_actions() -> dict[str, Callable]:
     """ forms actions dict available for vm_models"""
     return dict()
+
+
+def _fit(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _predict(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _initialize(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _drop(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _get_info(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _drop_fitting(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
+
+
+def _update(parameters: dict[str, Any]) -> dict[str, Any]:
+    pass
