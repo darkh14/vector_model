3f2c5262584e7938fa0aaa3080b05751dd999155
darkh14 <dark_h14@mail.ru>
2022-12-21 10:10:50 +0300

    general actions docstrings and type hinting reprocessed

--- actions.py
+++ actions.py
@@ -18,19 +18,32 @@ __all__ = ['get_actions']
 
 
 def get_actions() -> dict[str, Callable]:
-    """ forms general actions dict"""
+    """ forms general actions dict
+    :return: dict of function object (general actions)
+    """
     return dict({'test': _test, 'get_version': _get_version, 'ping': _ping})
 
 
 def _get_version(parameters: dict[str, Any]) -> str:
+    """
+    Returns version of vector_model
+    :param parameters: dict of request parameters
+    :return: version of module
+    """
     return get_version()
 
 
 def _test(parameters: dict[str, Any]) -> dict[str, Any]:
-    """ Action for testing and debugging """
+    """ Action for testing and debugging
+    :param parameters: dict of request parameters
+    :return: result of testing
+    """
     return test(parameters)
 
 
 def _ping(parameters: dict[str, Any]) -> dict[str, Any]:
-    """ Action for testing connection with service """
+    """ Action for testing connection with service
+    :param parameters: dict of request parameters
+    :return: result of checking connection
+    """
     return ping(parameters)
