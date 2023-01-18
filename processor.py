a1f900d7e9eb7dd0a5f209ae445a87bb62beacdb
darkh14 <dark_h14@mail.ru>
2023-01-18 15:01:59 +0300

    test json string 2

--- processor.py
+++ processor.py
@@ -170,14 +170,12 @@ class Processor(ABC):
 
             if content_length:
                 par_string = environ['wsgi.input'].read(content_length)
-                print('c_l - {}'.format(par_string))
+                print('c_l type - {}'.format(type(par_string)))
             else:
                 par_list = environ.get('wsgi.input')
                 if par_list:
                     for par_element in par_list:
                         par_string = par_element
-
-                print('p_l - {}'.format(par_string))
 
             if par_string:
                 request_parameters = self._parameters_from_json(par_string)
